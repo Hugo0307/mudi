e7a2971f5992cd29a0444c814ce586fba46335dc
hhenrique <hugohenrique20@gmail.com>
2022-08-30 23:09:32 -0300

    criando perfis de acesso

--- src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/br/com/alura/mvc/mudi/WebSecurityConfig.java
@@ -1,43 +1,43 @@
 package br.com.alura.mvc.mudi;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
 	
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().authenticated()
-			.and()
-			.formLogin(form -> form
+			.antMatchers("/").permitAll()
+			.antMatchers(HttpMethod.POST, "/login").permitAll()
+			.antMatchers("/home").hasAnyRole("USERS", "ADMINS")
+			.antMatchers("/pedidos").hasAnyRole("USERS", "ADMINS")
+			.antMatchers("/pedido/formulario").hasAnyRole("ADMINS")
+			.anyRequest().authenticated().and().formLogin(form -> form
 					.loginPage("/login")
 					.defaultSuccessUrl("/home")
 					.permitAll());
-			
-		return http.build();
 	}
 	
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("hugo")
-				.password("1234")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("user")
+				.password("{noop}user123")
+				.roles("USERS")
+				.and()
+				.withUser("admin")
+				.password("{noop}admin123")
+				.roles("ADMINS");
 	}
-
+	
 }
